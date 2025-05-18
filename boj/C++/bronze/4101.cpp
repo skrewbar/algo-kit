2ffbba5c73d542975c6c438a51a79c939b965766
tupa <hanss080@naver.com>
2025-05-18 16:02:05 +0900

    💩 4101 추가

---
+++ boj/C++/bronze/4101.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int a,b;
+    cin >> a >> b;
+
+    while(a || b){
+        if(a > b) cout <<"Yes\n";
+        else cout << "No\n";
+        cin >> a >> b;
+    }
+}
