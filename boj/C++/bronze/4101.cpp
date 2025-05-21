d35513b3ad038aaf0f35c4f8f76d0b62048376d4
skrewbar <skurua1244@gmail.com>
2025-05-21 22:14:36 +0900

    🎨 코드 포매팅

--- boj/C++/bronze/4101.cpp
+++ boj/C++/bronze/4101.cpp
@@ -1,16 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    int a,b;
+
+    int a, b;
     cin >> a >> b;
 
-    while(a || b){
-        if(a > b) cout <<"Yes\n";
-        else cout << "No\n";
+    while (a || b) {
+        if (a > b)
+            cout << "Yes\n";
+        else
+            cout << "No\n";
         cin >> a >> b;
     }
 }
