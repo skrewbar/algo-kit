92909ee6e7e0eb85b4af74a64e5b4ebf63de026a
skrewbar <skurua1244@gmail.com>
2025-06-15 19:34:22 +0900

    📝 C++ 14445번 풀이 작성

---
+++ boj/C++/gold/14445.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long n;
+    cin >> n;
+    cout << (n > 1 ? (n + 1) >> 1 : 0);
+
+    return 0;
+}
